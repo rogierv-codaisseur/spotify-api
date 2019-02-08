794a5802e7bea74a725b4cc4e396396c7f4a74e9
Rogier Verkaik <rogierv@gmail.com>
2019-02-08 22:24:34 +0100

    Add GET /playlists/:id: router

--- playlists/routes.js
+++ playlists/routes.js
@@ -6,7 +6,30 @@ const router = new Router();
 
 router.get('/playlists', (req, res, next) => {
   Playlist.findAll()
-    .then(playlists => res.status(201).send(playlists))
+    .then(playlists => {
+      // TODO: check of this condition is required:
+      if (!playlists || playlists.length === 0) {
+        return res.status(404).send({
+          message: `Playlists do not exist`
+        });
+      } else {
+        return res.status(201).send(playlists);
+      }
+    })
+    .catch(error => next(error));
+});
+
+router.get('/playlists/:id', (req, res, next) => {
+  Playlist.findByPk(req.params.id)
+    .then(playlist => {
+      if (!playlist) {
+        return res.status(404).send({
+          message: `Playlist does not exists`
+        });
+      } else {
+        return res.status(201).send(playlist);
+      }
+    })
     .catch(error => next(error));
 });
 
