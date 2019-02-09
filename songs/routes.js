407140848c27a83424ab3ac53cfd5cb1e39fd477
Rogier Verkaik <rogierv@gmail.com>
2019-02-09 13:04:22 +0100

    Implement change song information by User

--- songs/routes.js
+++ songs/routes.js
@@ -27,4 +27,36 @@ router.post('/playlists/:id/songs', auth, (req, res, next) => {
     .catch(error => next(error));
 });
 
+// PUT /playlists/:id/songs/:id: A user should be able to change song information, even move it to another playlist.
+
+router.put('/playlists/:id/songs/:songId', auth, (req, res, next) => {
+  const playlistId = req.params.id;
+  const songId = req.params.songId;
+
+  // Check if new playlist is valid
+  if (req.body.playlistId)
+    Playlist.findByPk(req.body.playlistId).then(newPlaylist => {
+      if (!newPlaylist)
+        return res.status(404).send({ message: 'Invalid new playlist' });
+    });
+
+  Playlist.findByPk(playlistId)
+    .then(playlist => {
+      if (!playlist || playlist.userId !== req.user.id)
+        return res.status(404).send({ message: 'Playlist does not exist' });
+      return Song.findOne({
+        where: { playlistId: playlist.id, id: songId }
+      })
+        .then(song => {
+          if (!song)
+            return res.status(404).send({ message: 'Song does not exist' });
+          return song
+            .update(req.body)
+            .then(changedSong => res.send(changedSong));
+        })
+        .catch(error => next(error));
+    })
+    .catch(error => next(error));
+});
+
 module.exports = router;
