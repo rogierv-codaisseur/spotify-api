2ffd9ced9b50c41859601485647bddc9c72c1a91
Rogier Verkaik <rogierv@gmail.com>
2019-02-09 15:21:17 +0100

    Add GET /artists WIP

--- artists/routes.js
+++ artists/routes.js
@@ -9,21 +9,37 @@ const router = new Router();
 
 // GET /artists: A user should be able to retrieve a list of artists, with all their songs (from the different playlists)
 
+router.get('/artists', (req, res, next) => {
+  Song.findAll({
+    attributes: [
+      Sequelize.fn('DISTINCT', Sequelize.col('artist')),
+      'artist',
+      'title'
+    ],
+    order: ['artist']
+  }).then(artist => res.send(artist));
+});
+
 // router.get('/artists', (req, res, next) => {
 //   Song.findAll({
-//     attributes: [
-//       'artist',
-//       [Sequelize.fn('Count', 'title'), 'titleCount'],
-//       'title'
-//     ],
-//     group: ['artist']
-//   }).then(artist => res.send(artist));
+//     group: ['artist'],
+//     attributes: [Sequelize.fn('COUNT', Sequelize.col('artist')), 'artist']
+//   }).then(result => res.send(result));
 // });
 
 // router.get('/artists', (req, res, next) => {
-//   Song.findAll({ where: { playlistId: 2 } })
+//   Song.findAll({ where: { playlistId: 1 } })
 //     .then(playlist => Playlist.findAll({ where: { id: playlist.playlistId } }))
 //     .then(result => res.send(result));
+// });
+
+// router.get('/artists', (req, res, next) => {
+//   Playlist.findAll({
+//     include: [Song],
+//     where: { userId: 1 }
+//   }).then(playlists => {
+//     return res.send({ playlists });
+//   });
 // });
 
 module.exports = router;
