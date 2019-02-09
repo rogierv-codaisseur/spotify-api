8e89cd748dc6685dd1fb9266e36433f75c78a3d7
Rogier Verkaik <rogierv@gmail.com>
2019-02-09 22:07:42 +0100

    Remove {force: false} from sync()

--- db.js
+++ db.js
@@ -9,7 +9,7 @@ const sequelize = new Sequelize(connectionString, {
 });
 
 sequelize
-  .sync({ force: false })
+  .sync()
   .then(() => {
     console.log('Sequelize updated database schema');
   })
